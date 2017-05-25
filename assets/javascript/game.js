6d40cb6a83ba2b7914fbe77037d076160c2b3003
matt <mtriantafellu@yahoo.com>
2017-05-24 22:04:46 -0400

    code as of 10pm

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,63 @@
 //Pseudo code
 
-//1  Define variables to be changed as game goes on
+//1  Define variables for arrays
 //2  The computer picks a random letter from the alphabet
 //2a define alphabet in array
 //2b setup random guess from computer
+//   var computerRandom = computerChoices[Math.floor(Math.random() *computerChoices.length)];
 //3  User presses key on keyboard
+//3a   Compare the computerRandom to the userGuess  
 //4  If the key pressed matches the one the computer selected add one to wins
 //5  If not add pressed key to losses and add the letter to the 'you guessed list'
 //6  If losses gets to 0 then reset
 //7  If win then reset
-/*
-//1  Define variables to be changed as game goes on
 
-  var wins = 0;
-  var losses = 0;
+var wins = 0;
+var losses = 0;
+var guesses = 9;
 
-//2  The computer picks a random letter from the alphabet
-*/
-//2.a  Our array of possible computer choices.
-  var computerChoices = ["a", "b", "c", "d",
-        "e", "f", "g", "h", "i", "j",
-        "k", "l", "m", "n", "o", "p",
-        "q", "r", "s", "t", "u", "v",
-        "w", "x", "y", "z"
+
+//Commented out most of the alphabet so that I could try and test it
+var computerChoices = ["a", "b"/*, "c", "d"*/
+//        , "e", "f", "g", "h", "i", "j",
+//        "k", "l", "m", "n", "o", "p",
+//       "q", "r", "s", "t", "u", "v",
+//        "w", "x", "y", "z"
         ];
 
-  var userChoices = ["a", "b", "c", "d",
-        "e", "f", "g", "h", "i", "j",
-        "k", "l", "m", "n", "o", "p",
-        "q", "r", "s", "t", "u", "v",
-        "w", "x", "y", "z"
-        ];
-/*
-//2.b  Sets the computerGuess variable equal to a random choice from the computerChoice array.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+//When user presses key run function
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+//randomly selecte letter out of computerChoices var above    
+    var computerRandom = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  console.log(computerRandom)
 
-//3  When the user presses a key, it will run the following function...
-      document.onkeyup = function(event) {
 
-// Determine which key was pressed
-        var userGuess = event.key;
+for (var i = 0; i < computerChoices.length; i++){
 
-// Sets the computerGuess variable equal to a random choice from the computerChoice array.
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  if (userGuess === computerChoices[i]) {
 
-// If the user presses a letter in the alphabet, run the game logic.
-        if (userChoice === computerGuess) {
-            wins++;          
-        } else if () }
-*/
+//if userGuess = computer randomly selected letter then add to wins
+    if (userGuess === computerRandom) {
+      wins++;
+    }  //if userGuess != computer randomly selected letter then add to losses
+     else if (userGuess != computerRandom) {
+      guesses--;
+    }
 
+//if guesses gets to 0 then add to losses count
+    if (guesses === 0) {
+      losses++;
+
+    }
+
+//as var wins/losses/guess change update in html document
+    document.querySelector("wins").innerHTML = wins;
+    document.querySelector("losses").innerHTML = losses;
+    document.querySelector("guessesLeft").innerHTML = guesses;
+  }
+}
+};
 
 // You Just Guessed:      WORKING
 
@@ -58,16 +66,4 @@ var guessesSoFar = document.getElementById("guessesSoFar");
 document.onkeyup = function(event) {
   console.log('event', event);
   guessesSoFar.textContent += event.key + ', ';
-  }
-
-function letterchecker(x) {
-  var userEntered = x.tolowercase().charAt(0);
-  var computerChose = x.tolowercase().charAt(i);
-
-  for (var i = 0; i < computerChoices.length; i++) {
-
-    console.log(computerChoices[i]);
-
-    } /* END OF FOR LOOP
-
-  } /* END OF FUNCTION LETTER CHECKER
+}
